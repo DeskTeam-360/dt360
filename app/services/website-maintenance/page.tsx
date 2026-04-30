cf422cfd41d8d8dca713e4348dd89c60aae484ee
hardyyudha <hardyyudha260@gmail.com>
2026-04-30 22:19:19 +0700

    add service/website-maintenance page

--- app/services/website-maintenance/page.tsx
+++ app/services/website-maintenance/page.tsx
@@ -1,9 +1,13 @@
+import { FAQ } from "@/components/pages/service/website-maintenance/FAQ";
+import { Hero } from "@/components/pages/service/website-maintenance/Hero";
+
 export default function WebsiteMaintenancePage() {
   return (
-    <main className="flex min-h-[60vh] items-center justify-center bg-white px-4 py-16">
-      <h1 className="text-4xl font-bold tracking-tight text-[#101651] sm:text-5xl">
-        Hello World
-      </h1>
+    <main className="bg-white">
+      <Hero />
+      <div className="-mt-px">
+        <FAQ />
+      </div>
     </main>
   );
 }
